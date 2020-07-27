850dd38e39d9d914b151e6495dfb44c0a1baadaa
Andrew R <45151082+andrewrubinstein@users.noreply.github.com>
2020-07-26 20:04:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ g++ -o a.out AscalMain.o Object.o AscalParameters.o
 rm AscalMain.o Object.o AscalParameters.o
 clear
 echo "Starting Ascal interpreter"
+echo "Running, awaiting input, try typing o;fibr(9)"
+echo "That will calculate the ninth fibonacci number, in the sequence, and show all the calculations,"
+echo "and branching paths it took to calculate the result."
 ./a.out
 
 
