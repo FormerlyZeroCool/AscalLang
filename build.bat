df947ba533ad7d08f4b0371898f4dec9e11b3ff4
andrewrubinstein <andrew.rubinstein93@gmail.com>
2020-08-17 20:48:10 -0400

    update build scripts

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
 echo "Building Ascal Interpreter with g++ -o"
 g++ -c AscalMain.cpp AscalFrame.hpp AscalParameters.hpp AscalParameters.cpp Object.hpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp
-del AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
 g++ -o ascal AscalMain.o Object.o AscalParameters.o   
 del AscalMain.o Object.o AscalParameters.o
 del AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
-echo clear
+clear
 echo "Starting Ascal interpreter"
 echo "Running, awaiting input, try typing o;fibr(3)"
 echo "That will calculate the third fibonacci number in the sequence 2, and show all the calculations"
