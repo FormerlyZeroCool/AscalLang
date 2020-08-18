b94ea7705afc49763f2f7b16ca551e9c3f5d1414
andrewrubinstein <andrew.rubinstein93@gmail.com>
2020-08-17 20:45:35 -0400

    Add build.bat to auto build, and run in windows with g++

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 echo "Building Ascal Interpreter with g++ -o"
 g++ -c AscalMain.cpp AscalFrame.hpp AscalParameters.hpp AscalParameters.cpp Object.hpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp
-rm AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
+del AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
 g++ -o ascal AscalMain.o Object.o AscalParameters.o   
-rm AscalMain.o Object.o AscalParameters.o
-rm AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
+del AscalMain.o Object.o AscalParameters.o
+del AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
 echo clear
 echo "Starting Ascal interpreter"
 echo "Running, awaiting input, try typing o;fibr(3)"
