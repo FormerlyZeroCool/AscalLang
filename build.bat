810ebf0ec392c6ca160a6d9f68202c55029126ce
andrewrubinstein <andrew.rubinstein93@gmail.com>
2020-10-23 20:49:27 -0400

    Update windows build file

--- build.bat
+++ build.bat
@@ -1,11 +1,10 @@
-echo "Building Ascal Interpreter with g++ -o"
-g++ -c AscalMain.cpp  AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp
-g++ -o ascal AscalMain.o Object.o AscalParameters.o   
-del AscalMain.o Object.o AscalParameters.o
-del AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch
-clear
-echo "Starting Ascal interpreter"
-echo "Running, awaiting input, try typing o;fibr(3)"
-echo "That will calculate the third fibonacci number in the sequence 2, and show all the calculations"
-echo "and branching paths it took to calculate the result."
-./ascal
+
+echo off 
+echo Building Ascal Interpreter with g++ -o
+echo g++ -O2  AscalMain.cpp  AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp
+g++ -O2  AscalMain.cpp  AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp
+echo Starting Ascal interpreter
+echo Running, awaiting input, try typing o;fibr(3)
+echo That will calculate the third fibonacci number in the sequence 2, and show all the calculations
+echo and branching paths it took to calculate the result.
+ascal.exe
