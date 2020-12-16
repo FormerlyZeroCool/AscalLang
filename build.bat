f85cbdcf181301c5e3b9cb87c7182c459df35811
andrewrubinstein <andrew.rubinstein93@gmail.com>
2020-12-16 00:30:07 -0500

    Bug fix compiling headers without need

--- build.bat
+++ build.bat
@@ -1,9 +1,8 @@
 
 echo off 
 echo Building Ascal Interpreter with g++ -O2
-echo g++ -O2 -std=c++17 AscalMain.cpp  AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp Calculator.hpp
-g++ -o ascal -O2  AscalMain.cpp  AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp ObjectKey.cpp Calculator.hpp
-
+echo g++ -O2 -std=c++17 AscalMain.cpp  AscalParameters.cpp Object.cpp 
+g++ -o ascal -O2  AscalMain.cpp  AscalParameters.cpp Object.cpp ObjectKey.cpp 
 echo Starting Ascal interpreter
 echo Running, awaiting input, try typing o;fibr(3)
 echo That will calculate the third fibonacci number in the sequence 2, and show all the calculations
--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 echo "Building Ascal Interpreter with g++ -O2"
-g++ -c -O2 -std=c++17 AscalMain.cpp AscalFrame.hpp AscalParameters.cpp Object.cpp queue.hpp setting.hpp stack.hpp unsortedlist.hpp ObjectKey.cpp Calculator.hpp
+g++ -c -O2 -std=c++17 AscalMain.cpp AscalFrame.hpp AscalParameters.cpp Object.cpp  ObjectKey.cpp 
 g++ -o ascal AscalMain.o Object.o AscalParameters.o ObjectKey.o
 rm AscalMain.o Object.o AscalParameters.o ObjectKey.o
-rm AscalFrame.hpp.gch AscalParameters.hpp.gch Object.hpp.gch queue.hpp.gch stack.hpp.gch unsortedlist.hpp.gch setting.hpp.gch Calculator.hpp.gch
 clear
 echo "Starting Ascal interpreter"
 echo "Running, awaiting input, try typing o;fibr(3)"
