98ad5a70e241768c4346437cbaabe2379060fd04
andrewrubinstein <andrew.rubinstein93@gmail.com>
2021-01-30 12:11:55 -0500

    bug fix string_view find add features to parsing util for string_view'

--- ParsingUtil.hpp
+++ ParsingUtil.hpp
@@ -13,6 +13,7 @@
 #include <sstream>
 #include "Calculator.hpp"
 #include "SubStr.hpp"
+#include "string_view.hpp"
 
 class AscalExecutor;
 static const std::string MAX = std::to_string(std::numeric_limits<double>::max());
@@ -30,23 +31,24 @@ public:
 	{
 		return c|32;
 	}
+	static double getNextDoubleS(const std::string &data,int &index);
 	static double getNextDouble(const std::string &data,int &index);
 	static std::string to_string(double input);
 	static SubStr getExpr(const std::string &data,int startingIndex, std::istream &ascal_cin, char opening = '{',char closing = '}',char lineBreak = ';');
 	static SubStr getFollowingExpr(std::string &exp, uint32_t startIndex,const std::string &id, char start = '(', char end = ')');
 	static SubStr getFollowingExpr(std::string &exp, uint32_t startIndex, const std::string &&id, char start = '(', char end = ')');
 	static SubStr getExprInString(const std::string &data,int index,char opening,char closing,char lineBreak);
+	static string_view getExprInStringSV(const std::string &data,uint32_t &index,char opening,char closing,char lineBreak);
 	static bool containsOperator(std::string s);
 	static SubStr getCodeBlock(std::string &exp, int index, std::istream &cin_ascal);
 	static SubStr getVarName(const std::string &s,int index);
+	static string_view getVarNameSV(const std::string &s,uint32_t &index);
 	static SubStr getNewVarName(const std::string &data);
 	//Generic string stuff
 	static bool cmpstr(const std::string &s1,const std::string &s2);
 	static bool cmpstr(const std::string &s1,const std::string &&s2);
 	static bool hasChar(const std::string &dat,const char &c);
 	static bool isDouble(std::string &exp);
-
-	static double getNextDoubleS(const std::string &data,int &index);
 	template <typename string_type>
 	static bool isObj(string_type &s)
 	{
--- string_view.hpp
+++ string_view.hpp
@@ -8,6 +8,7 @@
 #ifndef STRINGVIEW_HPP_
 #define STRINGVIEW_HPP_
 #include <string>
+#include <iostream>
 #if defined(WIN32) || defined(_WIN32)
 #include <cstring>
 #else
@@ -19,9 +20,9 @@ private:
 	uint32_t len = 0;
 public:
 	string_view();
-	string_view(std::string &s);
-	string_view(std::string &s, uint32_t start, uint32_t end);
-	string_view(char *s, uint32_t len);
+	string_view(const std::string &s);
+	string_view(const std::string &s, uint32_t start, uint32_t end);
+	string_view(const char *s, uint32_t len);
 	uint32_t size()
 	{
 		return this->len;
@@ -34,11 +35,12 @@ public:
 	{
 		return ptr[index];
 	}
-	uint32_t find(std::string &s);
-	uint32_t find(char *s, uint32_t start = 0, uint32_t size = -1);
+	uint32_t find(const std::string &s);
+	uint32_t find(const char *s, uint32_t start = 0, uint32_t size = -1);
 	uint32_t find(string_view, uint32_t start = 0, uint32_t size = -1);
 	string_view substr(uint32_t start, uint32_t end = -1);
 	std::string str();
+	friend std::ostream& operator<<(std::ostream &o,string_view &l);
 	virtual ~string_view();
 };
 
