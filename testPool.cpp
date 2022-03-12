487c35718a13e6d3697264e23363818418e95202
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-03-12 00:02:53 -0500

    add test file for pool allocator

---
+++ testPool.cpp
@@ -0,0 +1,26 @@
+#include "Pool.hpp"
+#include <vector>
+#include <iostream>
+int main()
+{
+    ObjectPool<long> a;
+    std::vector<long*> list;
+    for(int i = 0; i < 100000010; i++)
+        list.push_back(a.construct(i));
+    for(int i = 0; i < 100000000; i++){
+        a.destroy(list.back());
+        list.pop_back();
+    }
+    a.destroy(list[2]);
+    list.erase(list.begin() + 2);
+    for(int i = 0; i < 5; i++)
+        list.push_back(a.construct(i));
+
+    for(auto el:list)
+    //if(el)
+        std::cout<<*el<<" ";
+
+    
+    for(auto el:list)
+        a.destroy(el);
+}
