1a52a338787c3c4f3c4dd01bbb9462de384f13bd
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-03-13 15:56:25 -0400

    Bug fix delete object index calculation

--- keywords/DeleteObject.hpp
+++ keywords/DeleteObject.hpp
@@ -29,26 +29,26 @@ public:
 	    }
 	    else
 	    {
-	        std::string name = ParsingUtil::getVarName(frame->exp,7).data;
-
-	        if(runtime.memory.count(name) > 0)
+	        SubStr name = ParsingUtil::getVarName(frame->exp,this->keyWord.length() + 1);
+			frame->index = name.end + 1;
+	        if(runtime.memory.count(name.data) > 0)
 	        {
 
-	            std::vector<Object>::iterator position = std::find(runtime.userDefinedFunctions.begin(), runtime.userDefinedFunctions.end(), (runtime.memory)[name]);
+	            std::vector<Object>::iterator position = std::find(runtime.userDefinedFunctions.begin(), runtime.userDefinedFunctions.end(), (runtime.memory)[name.data]);
 	            if(position != runtime.userDefinedFunctions.end())
 	                runtime.userDefinedFunctions.erase(position);
 
-	            runtime.memory.erase(name);
+	            runtime.memory.erase(name.data);
 	    	    if(*runtime.boolsettings["o"])
-	            	std::cout<<"Erased "<<name<<" from memory."<<std::endl;
+	            	std::cout<<"Erased "<<name.data<<" from memory."<<std::endl;
 	        }
-	        else if(frame->getLocalMemory()->count(name) != 0)
+	        else if(frame->getLocalMemory()->count(name.data) != 0)
 	        {
-	            frame->getLocalMemory()->erase(name);
+	            frame->getLocalMemory()->erase(name.data);
 	        }
 	        else
 	        {
-	            throw std::string("Error could not find "+name+" in memory to delete.\n");
+	            throw std::string("Error could not find "+name.data+" in memory to delete.\n");
 	        }
 	    }
 	}
