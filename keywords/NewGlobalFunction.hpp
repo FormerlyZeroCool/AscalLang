c39b522546008bb25c2bcef9d21f29c279c43864
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-03-15 11:04:28 -0400

    Update to allow let f = [] to create array object

--- keywords/NewGlobalFunction.hpp
+++ keywords/NewGlobalFunction.hpp
@@ -26,11 +26,18 @@ public:
         Object *obj = nullptr;
         if(parent)
         {
-            if(ParsingUtil::firstChar(newVarPart.data, '\"')){
+            if(ParsingUtil::getFirstChar(newVarPart.data) == '\"'){
                 Object var(runtime.memMan, newVarPart.data,exPart.data);
                 //,frame->exp.substr(newVarPart.end + 1,exPart.start - 1).str()
                 var.compileInstructions();
                 obj = &parent->setList(var, atoi(var.getId().c_str()));
+            }
+            else if(ParsingUtil::getFirstChar(exPart.data) == '[')
+            {
+                Object var(runtime.memMan, newVarPart.data,exPart.data);
+                Object pushMethod(runtime.memMan, string_view("push"), string_view("arrPush(this,numberzxa)"));
+                obj = &parent->loadChild(var, runtime);
+                obj->loadChild(pushMethod, runtime);
             }
             else{
                 index = frame->index;
@@ -39,21 +46,25 @@ public:
                 obj = &parent->loadChild(var, runtime);
             }
         }
-        else if(runtime.memory.count(newVarPart.data) != 0)
-        {
-            std::vector<Object>::iterator position = std::find(runtime.userDefinedFunctions.begin(), runtime.userDefinedFunctions.end(), (runtime.memory)[newVarPart.data]);
-            if(position != runtime.userDefinedFunctions.end())
-                runtime.userDefinedFunctions.erase(position);
-            Object var(runtime.memMan, newVarPart.data,exPart.data);
-            //idk wut this is ,frame->exp.substr(newVarPart.end + 1,exPart.start - 1)
-            var.compileInstructions();
-            obj = &runtime.loadUserDefinedFn(var, runtime.memory);
-        }
         else
         {
+            if(runtime.memory.count(newVarPart.data) != 0)
+            {
+                std::vector<Object>::iterator position = std::find(runtime.userDefinedFunctions.begin(), runtime.userDefinedFunctions.end(), (runtime.memory)[newVarPart.data]);
+                if(position != runtime.userDefinedFunctions.end())
+                    runtime.userDefinedFunctions.erase(position);
+            }
             Object var(runtime.memMan, newVarPart.data,exPart.data);
+            string_view s = frame->exp.substr(exStart,frame->exp.length() - exStart);
+            std::cout<<s<<" "<<(s[0] == ' ')<<"  :"<<ParsingUtil::getFirstChar(s)<<": seee\n";
             var.compileInstructions();
             obj = &runtime.loadUserDefinedFn(var, runtime.memory);
+            if(ParsingUtil::getFirstChar(s) == '[')
+            {
+                Object pushMethod(runtime.memMan, string_view("push"), string_view("arrPush(this,numberzxa)"));
+                pushMethod.compileInstructions();
+                obj->loadChild(pushMethod, runtime);
+            }
         }
         if(*runtime.boolsettings["o"])
         {
