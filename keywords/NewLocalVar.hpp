71d48a6e88e8831b3865da7a2d809256a53d0a71
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-03-17 13:22:13 -0400

    Update local vars to be able to create arrays

--- keywords/NewLocalVar.hpp
+++ keywords/NewLocalVar.hpp
@@ -18,52 +18,63 @@ public:
     }
     void action(AscalFrame<double>* frame) override
     {
+        Object *nobj = nullptr;
         SubStr localName = ParsingUtil::getVarName(frame->exp,frame->exp.find("loc",frame->index)+4);
         static uint32_t startOfExp;
         startOfExp = frame->exp.find('=', frame->index)+1;
-        SubStr subexp = ParsingUtil::getExpr(frame->exp, startOfExp, runtime.ascal_cin);
-        if(!ParsingUtil::isObj(subexp.data))
+
+        if(ParsingUtil::getFirstChar(frame->exp.substr(startOfExp)) == '[')
         {
-            Object *nobj = nullptr;
-            if(!ParsingUtil::isDouble(subexp.data))
-            {
-                Object newLocal = Object(runtime.memMan, localName.data,subexp.data,"");
-                newLocal.compileInstructions();
-                nobj = &runtime.loadUserDefinedFn(newLocal, *frame->getLocalMemory());
-            }
-            else
-            {
-                Object obj(runtime.memMan, localName.data);
-                const uint32_t nullIndex = ParsingUtil::isNumeric(subexp.data[subexp.data.size()-1])?subexp.data.size():subexp.data.size()-1;
-                char tmp = subexp.data[nullIndex];
-                subexp.data[nullIndex] = 0;
-                obj.setDouble(atof(&subexp.data[0]));
-                subexp.data[nullIndex] = tmp;
-                nobj = &runtime.loadUserDefinedFn(obj, *frame->getLocalMemory());
-            }
-            if(*runtime.boolsettings["o"])
-            {
-                std::cout<<std::endl<<"New Local function: "<<localName.data<< " exp: "<<nobj->instructionsToFormattedString()<<std::endl;
-            }
+            Object obj(runtime.memMan, localName.data);
+            nobj = &runtime.loadUserDefinedFn(obj, *frame->getLocalMemory());
+            runtime.makeArray(*nobj);
         }
         else
         {
-            SubStrSV rightVarStart = ParsingUtil::getVarNameSV(frame->exp,startOfExp);
-            Object *newVar = runtime.resolveNextObjectExpression(frame, rightVarStart).data;
-            if(newVar)
+            SubStr subexp = ParsingUtil::getExpr(frame->exp, startOfExp, runtime.ascal_cin);
+            if(!ParsingUtil::isObj(subexp.data))
             {
-                runtime.loadUserDefinedFn(*newVar, *frame->getLocalMemory());
+                if(!ParsingUtil::isDouble(subexp.data))
+                {
+                    Object newLocal = Object(runtime.memMan, localName.data,subexp.data,"");
+                    newLocal.compileInstructions();
+                    nobj = &runtime.loadUserDefinedFn(newLocal, *frame->getLocalMemory());
+                }
+                else
+                {
+                    Object obj(runtime.memMan, localName.data);
+                    const uint32_t nullIndex = ParsingUtil::isNumeric(subexp.data[subexp.data.size()-1])?subexp.data.size():subexp.data.size()-1;
+                    char tmp = subexp.data[nullIndex];
+                    subexp.data[nullIndex] = 0;
+                    obj.setDouble(atof(&subexp.data[0]));
+                    subexp.data[nullIndex] = tmp;
+                    nobj = &runtime.loadUserDefinedFn(obj, *frame->getLocalMemory());
+                }
+                if(*runtime.boolsettings["o"])
+                {
+                    std::cout<<std::endl<<"New Local function: "<<localName.data<< " exp: "<<nobj->instructionsToFormattedString()<<std::endl;
+                }
             }
             else
             {
-                Object newLocal = Object(runtime.memMan, localName.data,subexp.data,"");
-                newLocal.compileInstructions();
-                runtime.loadUserDefinedFn(newLocal, *frame->getLocalMemory());
+                SubStrSV rightVarStart = ParsingUtil::getVarNameSV(frame->exp,startOfExp);
+                Object *newVar = runtime.resolveNextObjectExpression(frame, rightVarStart).data;
+                if(newVar)
+                {
+                    runtime.loadUserDefinedFn(*newVar, *frame->getLocalMemory());
+                }
+                else
+                {
+                    Object newLocal = Object(runtime.memMan, localName.data,subexp.data, "");
+                    newLocal.compileInstructions();
+                    runtime.loadUserDefinedFn(newLocal, *frame->getLocalMemory());
+                }
+                if(*runtime.boolsettings["o"])
+                {
+                    std::cout<<std::endl<<"New Local function: "<<localName.data<< " exp: "<<newVar->toString()<<std::endl;
+                }
             }
-            if(*runtime.boolsettings["o"])
-            {
-                std::cout<<std::endl<<"New Local function: "<<localName.data<< " exp: "<<newVar->toString()<<std::endl;
-            }
+
         }
 
     }
