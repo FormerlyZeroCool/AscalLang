0bca6571576de53b8755284c1f859bdce90c4c59
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-03-25 14:36:08 -0400

    Add linux build to use readline by default

--- linux-build.sh
+++ linux-build.sh
@@ -1,5 +1,5 @@
 echo "Building Ascal Interpreter with g++ -O2"
-g++  -c -Wall -std=c++2a -O3 -I/usr/include/readline AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp CrossPlatform.cpp ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp 
+g++  -c -Wall -std=c++2a -O3 -Dposix -I/usr/include/readline AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp CrossPlatform.cpp ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp 
 #g++  -Wall -std=c++20 -Og -ggdb3 -fsanitize=address,undefined -I/usr/local/opt/readline/include -L/usr/local/opt/readline/lib -lreadline AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp CrossPlatform.cpp ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp
 g++ -o ascal -O3 AscalMain.o Object.o AscalParameters.o string_view.o Ascal.o AscalExecutor.o CrossPlatform.o ParsingUtil.o PRNG.o Keyword.o MemoryMap.o MemoryManager.o SubStr.o -L/usr/lib/x86_64-linux-gnu -lreadline
 #rm AscalMain.o Object.o AscalParameters.o string_view.o Ascal.o AscalExecutor.o CrossPlatform.o ParsingUtil.o PRNG.o Keyword.o MemoryMap.o MemoryManager.o SubStr.o -L/usr/lib/x86_64-linux-gnu -lreadline
