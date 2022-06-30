d48b8180d58fcb2bc8e8705db59959d52246cc45
andrewrubinstein <andrew.rubinstein93@gmail.com>
2022-06-29 21:45:16 -0400

    Update build.sh to use std 2a

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Building Ascal Interpreter with g++ -O2"
-g++ -c -Wall -std=c++20 -O3 -Dlibreadline -Ddebug4  -I/usr/local/Cellar/boost/1.76.0/include -I/usr/local/opt/readline/include AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp  ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp 
+g++ -c -Wall -std=c++2a -O3 -Dlibreadline -Ddebug3  -I/usr/local/Cellar/boost/1.76.0/include -I/usr/local/opt/readline/include AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp  ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp 
 g++ -o ascal  -L/usr/local/opt/readline/lib -lreadline -O3 AscalMain.o Object.o AscalParameters.o string_view.o Ascal.o AscalExecutor.o ParsingUtil.o PRNG.o Keyword.o MemoryMap.o MemoryManager.o SubStr.o
 #clear
 #g++  -Wall -std=c++20 -Og -ggdb3 -Dlibreadline -Ddebug -fsanitize=address,undefined -I/usr/local/opt/readline/include -L/usr/local/opt/readline/lib -lreadline AscalMain.cpp AscalParameters.cpp Object.cpp   string_view.cpp Ascal.cpp AscalExecutor.cpp  ParsingUtil.cpp PRNG.cpp Keyword.cpp MemoryMap.cpp MemoryManager.cpp SubStr.cpp
